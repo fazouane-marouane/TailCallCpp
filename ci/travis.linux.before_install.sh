59f856eeb074832531a2d10989c8258c9a4dd0cf
Fazouane Marouane <fazouanem3@gmail.com>
2015-10-09 23:22:01 +0200

    ...so close

--- ci/travis.linux.before_install.sh
+++ ci/travis.linux.before_install.sh
@@ -1,25 +1,19 @@
 #!/bin/bash
 set -ev
 if [ "$CXX" == "g++" ]; then
-  export MY_CXX_Compiler=g++-${MY_GCC_VERSION};
+  MY_CXX_Compiler=g++-${MY_GCC_VERSION};
 fi
 if [ "$CXX" == "clang++" ]; then
-  export MY_CXX_Compiler=clang++-${MY_CLANG_VERSION};
+  MY_CXX_Compiler=clang++-${MY_CLANG_VERSION};
 fi
 if [ "$CC" == "gcc" ]; then
-  export MY_CC_Compiler=gcc-${MY_GCC_VERSION};
+  MY_CC_Compiler=gcc-${MY_GCC_VERSION};
 fi
 if [ "$CC" == "clang" ]; then
-  export MY_CC_Compiler=clang-${MY_CLANG_VERSION};
+  MY_CC_Compiler=clang-${MY_CLANG_VERSION};
 fi
 echo ${TRAVIS_OS_NAME}
 echo ${CXX}
 echo ${CC}
 echo ${MY_GCC_VERSION}
 echo ${MY_CLANG_VERSION}
-mkdir build
-cd build
-${MY_CXX_Compiler} --version
-${MY_CC_Compiler} --version
-cmake --version
-cmake -DCMAKE_BUILD_TYPE=${BUILD_TYPE} -DCMAKE_CXX_COMPILER=${MY_CXX_Compiler} -DCMAKE_C_COMPILER=${MY_CC_Compiler} ..
