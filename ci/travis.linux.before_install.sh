69c7ebc210d137df52ea51f5f0647711df95e5fd
Fazouane Marouane <fazouanem3@gmail.com>
2015-10-09 22:15:53 +0200

    test

--- ci/travis.linux.before_install.sh
+++ ci/travis.linux.before_install.sh
@@ -2,10 +2,13 @@
 set -ev
 if [ "$CXX" == "g++" ]; then
   export MY_CXX_Compiler=$(which g++-${MY_GCC_VERSION});
+fi
+if [ "$CXX" == "g++" ]; then
   export MY_CC_Compiler=$(which gcc-${MY_GCC_VERSION});
 fi
-
 if [ "$CXX" != "g++" ]; then
   export MY_CXX_Compiler=$(which clang++-${MY_CLANG_VERSION});
+fi
+if [ "$CXX" != "g++" ]; then
   export MY_CC_Compiler=$(which clang-${MY_CLANG_VERSION});
 fi
--- ci/travis.osx.before_install.sh
+++ ci/travis.osx.before_install.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 set -ev
-brew update;
-brew update clang;
-export MY_CC_Compiler=${CXX};
-export MY_CC_Compiler=${CC};
+brew update
+brew update clang
+export MY_CC_Compiler=${CXX}
+export MY_CC_Compiler=${CC}
