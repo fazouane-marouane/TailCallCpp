eb511010ba5a6878647b74fb604fffd9eac7f4ff
Fazouane Marouane <fazouanem3@gmail.com>
2015-10-09 21:56:20 +0200

    ci

---
+++ ci/travis.linux.before_install.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+set -ev
+if [ "$CXX" == "g++" ]; then
+  export MY_CXX_Compiler=$(which g++-${MY_GCC_VERSION});
+  export MY_CC_Compiler=$(which gcc-${MY_GCC_VERSION});
+fi
+
+if [ "$CXX" != "g++" ]; then
+  export MY_CXX_Compiler=$(which clang++-${MY_CLANG_VERSION});
+  export MY_CC_Compiler=$(which clang-${MY_CLANG_VERSION});
+fi
