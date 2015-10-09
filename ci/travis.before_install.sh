ff9ab94cf24bcf967b0d34999b658e170e81c39a
Fazouane Marouane <fazouanem3@gmail.com>
2015-10-09 22:08:01 +0200

    test

--- ci/travis.before_install.sh
+++ ci/travis.before_install.sh
@@ -1,9 +1,11 @@
 #!/bin/bash
 if [ -z "${TRAVIS_OS_NAME}" ] || [ "${TRAVIS_OS_NAME}" = "linux" ]; then
   echo Before install on linux.
+  chmod a+x ./ci/travis.linux.before_install.sh;
   ./ci/travis.linux.before_install.sh;
 fi
 if [ "${TRAVIS_OS_NAME}" = "osx" ]; then
   echo Before install on OSX.
+  chmod a+x ./ci/travis.osx.before_install.sh;
   ./ci/travis.osx.before_install.sh;
 fi
