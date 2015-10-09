9a4e0e6a2f70baaa2a0d09d23dfa1f8a80aca9fe
Fazouane Marouane <fazouanem3@gmail.com>
2015-10-09 23:07:27 +0200

    t

--- ci/travis.linux.before_install.sh
+++ ci/travis.linux.before_install.sh
@@ -1,17 +1,22 @@
 #!/bin/bash
 set -ev
-if [ "${CXX_Cmp}" == "g++" ]; then
+if [ "$CXX_Cmp" == "g++" ]; then
   export MY_CXX_Compiler=$(which g++-${GCC_Cmp});
 fi
-if [ "${CXX_Cmp}" == "clang++" ]; then
+if [ "$CXX_Cmp" == "clang++" ]; then
   export MY_CXX_Compiler=$(which clang++-${CLANG_Cmp});
 fi
-if [ "${CC_Cmp}" == "gcc" ]; then
+if [ "$CC_Cmp" == "gcc" ]; then
   export MY_CC_Compiler=$(which gcc-${GCC_Cmp});
 fi
-if [ "${CC_Cmp}" == "clang" ]; then
+if [ "$CC_Cmp" == "clang" ]; then
   export MY_CC_Compiler=$(which clang-${CLANG_Cmp});
 fi
+echo ${TRAVIS_OS_NAME}
+echo ${CXX_Cmp}
+echo ${CC_Cmp}
+echo ${GCC_Cmp}
+echo ${CLANG_Cmp}
 mkdir build
 cd build
 ${MY_CXX_Compiler} --version
