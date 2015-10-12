93e9ee5c4fd6cceb64d465ca2f4290c17bd71741
Fazouane Marouane <fazouanem3@gmail.com>
2015-10-12 03:57:12 +0200

    heavy refactoring: clearer/simpler usage

--- main.cpp
+++ main.cpp
@@ -1,76 +1,34 @@
 #include <iostream>
 #include <string>
-#include <boost/variant.hpp>
-#include <functional>
+#include "src/tail_call.h"
 
-
-template<typename TResult, typename... TArgs>
-class TailRec
+int factorial(int n)
 {
-public:
-	using inner_t = std::function<boost::variant<TResult, boost::recursive_wrapper<TailRec<TResult>>>(TArgs...)>;
-
-	TailRec(inner_t _f) : f(_f) {}
-	boost::variant<TResult, TailRec<TResult>> operator()(TArgs... args) const
-	{
-		return f(args...);
-	}
-private:
-	inner_t f;
-};
-
-template<typename TResult, typename... TArgs>
-using TailRec_t = std::function<std::function<boost::variant<TResult, TailRec<TResult>>(TArgs...)>(TailRec<TResult, TArgs...>)>;
-
-
-template<typename TResult, typename... TArgs>
-class TailRecursiveYCombinator
-{
-public:
-	std::function<TResult(TArgs...)> operator()(TailRec_t<TResult, TArgs...> f)
-	{
-		return [f](TArgs... args) -> TResult {
-			auto rec = [&f](auto& r) -> std::function<boost::variant<TResult, TailRec<TResult>>(TArgs...)> {
-				return [&f, &r](auto... _args) -> boost::variant<TResult, TailRec<TResult>> {
-					return TailRec<TResult>(std::function<boost::variant<TResult, TailRec<TResult>>()>([&f, &r, _args...] { return (f(TailRec<TResult, TArgs...>(r(r))))(_args...); }));
-				};
-			};
-			auto _f = rec(rec);
-			auto r = _f(args...);
-			while (r.which() == 1)
-			{
-				r = (boost::get<TailRec<TResult>>(r))();
-			}
-			return boost::get<TResult>(r);
-		};
-	}
-};
-
-int factorial(int n, int r)
-{
-	return n<2 ? r : factorial(n - 1, n*r + 1);
+  return n<2 ? 1 : n*factorial(n - 1);
 }
 
-int factorial2(int n)
+int tailcall_factorial(int n, int r)
 {
-	return n<2 ? 1 : n*factorial2(n - 1) + 1;
+  return n<2 ? r : factorial(n - 1, n*r);
 }
 
 int main()
 {
-	auto g = TailRecursiveYCombinator<int, int, int>();
-	auto h = [](TailRec<int, int, int> fact) {
-		return [=](int n, int r) {
-			return n < 2 ? r : fact(n - 1, n*r + 1);
+  using namespace TailCallCpp;
+  auto MyFactorial = TailCallCpp::RecWrapper<int(int, int)>([](auto& fact) {
+    return [=](int n, int r) {
+      return n < 2 ? r : fact(n - 1, n*r);
+    };
+  });
 
-		};
-	};
+  std::cout << MyFactorial(5, 1) << std::endl;
+  std::cout << factorial(5) << std::endl;
+  std::cout << tailcall_factorial(5, 1) << std::endl;
 
-	const int N = 1000000;
 
-	std::function<int(int, int)> Factorial2 = g(h);
-	std::cout << Factorial2(N, 1) << std::endl;
-	//std::cout << factorial(N, 1) << std::endl;
-	//std::cout << factorial2(N) << std::endl;
+  //const int N = 1000000;
+  //std::cout << MyFactorial(N, 1) << std::endl; // never segfaults thanks to tail call optimization :)
+  //std::cout << factorial(N) << std::endl; // will segfault (due to recursivity)
+  //std::cout << tailcall_factorial(N, 1) << std::endl; // no tail call optimization :( also segfaults
 
 }
---
+++ src/tail_call.h
@@ -0,0 +1,80 @@
+#pragma once
+#ifndef TailCallCpp_TailCall_H
+#define TailCallCpp_TailCall_H
+#include <boost/variant.hpp>
+#include <functional>
+
+namespace TailCallCpp
+{
+  namespace details
+  {
+    template<typename T>
+    class TailRecursive_wrapper;
+
+    template<typename TResult, typename... TArgs>
+    class TailRecursive_wrapper<TResult(TArgs...)>
+    {
+    public:
+      using type = std::function<boost::variant<TResult, boost::recursive_wrapper<TailRecursive_wrapper<TResult()>>>(TArgs...)>;
+
+      TailRecursive_wrapper(type _f) : f(_f) {}
+      boost::variant<TResult, TailRecursive_wrapper<TResult()>> operator()(TArgs... args) const
+      {
+        return f(args...);
+      }
+    private:
+      type f;
+    };
+
+    template<typename T>
+    using TailRecursiveDefinition = std::function<typename TailRecursive_wrapper<T>::type(TailRecursive_wrapper<T> const&)>;
+
+    template<typename T>
+    class TailRecursiveYCombinator;
+
+    template<typename TResult, typename... TArgs>
+    class TailRecursiveYCombinator<TResult(TArgs...)>
+    {
+    public:
+      static std::function<TResult(TArgs...)> create(TailRecursiveDefinition<TResult(TArgs...)> const& f)
+      {
+        return [f](TArgs... args) -> TResult {
+          auto rec = [&f](auto& r) -> typename TailRecursive_wrapper<TResult(TArgs...)>::type {
+            return [&f, &r](auto... _args) -> boost::variant<TResult, TailRecursive_wrapper<TResult()>> {
+              return TailRecursive_wrapper<TResult()>([&f, &r, _args...]{ return (f(r(r)))(_args...); });
+            };
+          };
+          auto _f = rec(rec);
+          auto r = _f(args...);
+          while (r.which() == 1)
+          {
+            r = (boost::get<TailRecursive_wrapper<TResult()>>(r))();
+          }
+          return boost::get<TResult>(r);
+        };
+      }
+    };
+  }
+
+  template<typename T>
+  class RecWrapper;
+
+  template<typename TResult, typename... TArgs>
+  class RecWrapper<TResult(TArgs...)>
+  {
+  public:
+    template<typename S>
+    RecWrapper(S&& _f) : f(details::TailRecursiveYCombinator<TResult(TArgs...)>::create(
+      [_f](details::TailRecursive_wrapper<TResult(TArgs...)> const& rec) { return _f(rec); })) {}
+
+    TResult operator()(TArgs... args) const
+    {
+      return f(args...);
+    }
+
+  private:
+    std::function<TResult(TArgs...)> f;
+  };
+}
+
+#endif // TailCallCpp_TailCall_H
